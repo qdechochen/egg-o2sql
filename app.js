790a9413b28100c513e952d1fa932bab7ea96552
Echo Chen <echo.chen@umoregroup.com>
2019-04-25 16:28:29 +0800

    add client support for transaction

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 const { Pool, Client } = require('pg');
 const o2sql = require('o2sql');
 
+function capFirstLetter(s) {
+  return s[0].toUpperCase() + s.substring(1);
+}
 const queryWithObjectReplacer = (() => {
   return (template, data) => {
     const values = [];
@@ -33,21 +36,60 @@ function createPgClient(config, app) {
       text,
       values,
     });
-    const result = await (client ? client : pool).query({ text, values });
+    let result = await (client ? client : pool).query({ text, values });
+    let columns;
     if (this.command === 'select') {
-      if (this.isGet) {
-        return result.rows.length > 0 ? result.rows[0] : null;
-      } else if (this.isCount) {
-        return result.rows[0].count;
+      if (this.isCount) {
+        columns = null;
+        result = result.rows[0].count;
+      } else {
+        columns = this._columns;
+        result = result.rows;
       }
-      return result.rows;
-    } else if (this.command === 'insert') {
-      return result.rows[0];
-    } else if (this.command === 'update') {
-      return result.rows[0];
-    } else if (this.command === 'delete') {
-      return result;
+    } else if (this.command === 'insert' || this.command === 'update') {
+      result = result.rows;
+      columns = this._returning;
+    } /* else if (this.command === 'delete') {
+      result = result;
+    }*/
+    if (columns) {
+      const groups = columns.filter(t => t.group);
+      if (groups.length > 0) {
+        groups.forEach(g => {
+          const group = {};
+          g.fields = g.fields.map(f => {
+            if (f instanceof Array) {
+              const [column, alias] = column;
+              f = alias || column;
+            }
+            return [
+              g.prefix
+                ? g.prefix + (g.separator ? g.separator + f : capFirstLetter(f))
+                : f,
+              f,
+            ];
+          });
+        });
+
+        result.forEach(r => {
+          groups.forEach(g => {
+            r[g.prefix] = {};
+            g.fields.forEach(f => {
+              r[g.prefix][f[1]] = r[f[0]];
+              delete r[f[0]];
+            });
+          });
+        });
+      }
     }
+
+    if (this.command === 'insert' || this.command === 'update') {
+      result = result[0];
+    } else if (this.command === 'select' && this.isGet) {
+      result = result.length > 0 ? result[0] : null;
+    }
+
+    return result;
   });
   app.o2sql = o2sql;
 
@@ -59,41 +101,46 @@ function createPgClient(config, app) {
 
   async function query(text, vals, client) {
     console.log('[egg-pg] query');
-    console.dir({text, vals});
+    console.dir({ text, vals });
     return await (client ? client : pool).query(text, vals);
   }
 
-  async function transaction(queries) {
-    const client = await pool.connect();
+  async function transaction(queries, client) {
+    if (!client) {
+      client = await pool.connect();
 
-    let result,
-      error;
-    try {
-      await client.query('BEGIN');
-      console.log('TRANSACTION BEGINS.............');
+      let result, error;
+      try {
+        await client.query('BEGIN');
+        console.log('TRANSACTION BEGINS.............');
 
-      result = await queries(client);
+        result = await queries(client);
 
-      await client.query('COMMIT');
-      console.log('TRANSACTION COMMITTED.............');
-    } catch (e) {
-      console.dir(e);
-      error = e;
-      await client.query('ROLLBACK');
-      console.log('TRANSACTION ROLLBACK.............');
-    } finally {
-      client.release();
-    }
-    if (error) {
-      throw error;
+        await client.query('COMMIT');
+        console.log('TRANSACTION COMMITTED.............');
+      } catch (e) {
+        console.dir(e);
+        error = e;
+        await client.query('ROLLBACK');
+        console.log('TRANSACTION ROLLBACK.............');
+      } finally {
+        client.release();
+      }
+      if (error) {
+        throw error;
+      } else {
+        return result;
+      }
     } else {
-      return result;
+      return await queries(client);
     }
   }
 
   app.beforeStart(async () => {
     const { rows } = await query('select now() as "currentTime"');
-    console.log(`[egg-pg] instance status OK, rds currentTime: ${rows[0].currentTime}`);
+    console.log(
+      `[egg-pg] instance status OK, rds currentTime: ${rows[0].currentTime}`
+    );
   });
 
   return {
--- readme.md
+++ readme.md
@@ -1,13 +1,17 @@
 # egg o2sql plugin with pg integreted
 
 ## Install
+
 ```bash
 $ npm i egg-o2sql --save
 ```
 
 ## Configuration
+
 ### Enable egg-o2sql
+
 `{app_root}/config/plugin.js`:
+
 ```js
 exports.o2sql = {
   enable: true,
@@ -16,7 +20,9 @@ exports.o2sql = {
 ```
 
 ### pg settings
+
 `${app_root}/config/config.default.js`:
+
 ```js
 exports.pg = {
   default: {
@@ -28,20 +34,46 @@ exports.pg = {
   },
 };
 ```
+
 Refer to https://node-postgres.com for details.
 
 ## Usage
+
 ```js
 const products = await app.o2sql
   .select(['id', 'name', 'price'])
   .from('product')
   .where({
     id: {
-      IN: o2sql.select(['id']).from('productCat').where({ catId: 1 }),
+      IN: o2sql
+        .select(['id'])
+        .from('productCat')
+        .where({ catId: 1 }),
     },
   })
   .pagination(2, 10)
   .orderby(['id'])
   .execute();
 ```
+
+```js
+const result = await app.pg.transition(async client => {
+  const products = await app.o2sql
+    .select(['id', 'name', 'price'])
+    .from('product')
+    .where({
+      id: {
+        IN: o2sql.select(['id']).from('productCat').where({ catId: 1 }),
+      },
+    })
+    .pagination(2, 10)
+    .orderby(['id'])
+    .execute(client);
+  await app.o2sql
+    ....
+    .execute(client);
+  return products;
+});
+```
+
 Refer to https://github.com/qdechochen/o2sql for o2sql details.
